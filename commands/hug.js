3372c768d947ec3deba1b4a6c15d05f22bb2bd84
Jonchu650 <68526622+Jonchu650@users.noreply.github.com>
2020-09-29 08:45:18 -0700

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -5,6 +5,7 @@ module.exports = {
     usage: '<user>',
     execute(message, args) {
         const huggedUser = message.mentions.users.first();
+        if (!huggedUser) return message.channel.send('Ping a user to hug them!')
         if (huggedUser.id === '750573070669840475') {
             message.channel.send(':flushed:')
         } else if (huggedUser === message.author) {
