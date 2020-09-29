0a50e31f94d49098fd35906514af9be74b749d69
Jonchu650 <68526622+Jonchu650@users.noreply.github.com>
2020-09-29 09:31:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13,16 +13,17 @@ for (const file of commandFiles) {
 }
 client.once('ready', () => {
 	antispam(client, {
-        limitUntilWarn: 5, // The amount of messages allowed to send within the interval(time) before getting a warn.
-        limitUntilMuted: 7, // The amount of messages allowed to send within the interval(time) before getting a muted.
-        interval: 10000, // The interval(time) where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted. (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc etc)
+        limitUntilWarn: 10, // The amount of messages allowed to send within the interval(time) before getting a warn.
+        limitUntilMuted: 15, // The amount of messages allowed to send within the interval(time) before getting a muted.
+        interval: 7000, // The interval(time) where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted. (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc etc)
         warningMessage: "if you don't stop from spamming, I'm going to mute you!", // Message you get when you are warned!
         muteMessage: "Was muted for spam.", // Message sent after member X was punished(muted).
-        maxDuplicatesWarning: 7,// When people are spamming the same message, this will trigger when member X sent over 7+ messages.
+        maxDuplicatesWarning: 6,// When people are spamming the same message, this will trigger when member X sent over 7+ messages.
         maxDuplicatesMute: 10, // The limit where member X get muted after sending too many messages(10+).
         ignoredRoles: ["♡Admin♡"], // The members with this role(or roles) will be ignored if they have it. Suggest to not add this to any random guys. Also it's case sensitive.
         mutedRole: "Muted", // Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
-        timeMuted: 1000 * 600 // This is how much time member X will be muted. if not set, default would be 10 min.
+        timeMuted: 1000 * 600, // This is how much time member X will be muted. if not set, default would be 10 min.
+	logChannel: "logs"
       });
 	console.log(`${client.user.tag} Is now online.`); //says this when it goes back up
 
