fd6e9025929c08771ab2122bf5e4224b3ffb1015
Jonchu650 <68526622+Jonchu650@users.noreply.github.com>
2020-10-01 07:51:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -210,7 +210,7 @@ client.on('guildMemberRemove', async member => {
 
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'goodbye-image.png');
 
-	channel.send(`Cya soon, **${member.tag}**!`, attachment).then(sentMessage => {
+	channel.send(`Cya soon, **${member.user.tag}**!`, attachment).then(sentMessage => {
 	sentMessage.react('748627665233248347');
 	});
 });
